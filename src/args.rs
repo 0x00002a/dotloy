985e5a6ee5edb30808170176e8d98e6173cce7cc
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-05-18 18:50:00 +0100

    chore: rename to dotloy

--- src/args.rs
+++ src/args.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 
 #[derive(Parser)]
 pub struct Args {
-    #[arg(help = "Config file to use. If not provided defaults to dotoy.yaml in cwd")]
+    #[arg(help = "Config file to use. If not provided defaults to dotloy.yaml in cwd")]
     pub config: Option<std::path::PathBuf>,
 
     #[arg(long, help = "Print actions but don't actually do them")]
